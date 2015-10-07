9b81ecebfbf793af00066db383101ec1325c220f
Sam Corcos <sam@corcos.io>
2015-10-07 14:46:12 -0700

    adding unique not proposed

--- cato.js
+++ cato.js
@@ -1,86 +1,81 @@
 if (Meteor.isClient) {
   Meteor.startup(function() {
-    var width = 900
-    var height = 300
+    (function() {
+      var width = 900
+      var height = 300
 
-    var color = d3.scale.linear()
-      .domain([0, 0.5, 1])
-      .range(["red", "white", "blue"]);
+      var color = d3.scale.linear()
+        .domain([0, 0.5, 1])
+        .range(["red", "white", "blue"]);
 
-    var div = d3.select("body").append("div")
-        .attr("class", "tooltip")
-        .style("opacity", 0);
+      var div = d3.select("body").append("div")
+          .attr("class", "tooltip")
+          .style("opacity", 0);
 
-    var svg = d3.select('body').append('svg')
-      .attr("width", width)
-      .attr("height", height)
-      .attr("class", "background")
+      var svg = d3.select('body').append('svg')
+        .attr("width", width)
+        .attr("height", height)
+        .attr("class", "background")
 
-    let centerLine = svg.append("rect")
-      .attr("width", width)
-      .attr("height", "2px")
-      .attr("x", "0px")
-      .attr("y", height / 2 + "px")
-      .attr("class", "center-line")
+      var centerLine = svg.append("rect")
+        .attr("width", width)
+        .attr("height", "2px")
+        .attr("x", "0px")
+        .attr("y", height / 2 + "px")
+        .attr("class", "center-line")
 
 
-    d3.json('federalBodies.json', function(err, acts) {
-      if (err) {throw err};
-      // console.log(acts);
+      d3.json('acts-unique-notproposed.json', function(err, acts) {
+        if (err) {throw err};
+        // console.log(acts);
 
-      var node = svg.selectAll(".node")
-        .data(acts)
-      .enter().append("circle")
-        .attr({
-          cx: function(d) {
-            var sum = d.Republican + d.Democrat
-            var percentD = d.Democrat / sum
-            var scaled = d3.scale.linear()
-              .domain([0, 1])
-              .range([100, width - 100])
-            return scaled(percentD) + "px"
-          },
-          cy: height / 2 + "px",
-          r: function(d) { return (d.Republican + d.Democrat) * 4 },
-          class: 'node'
-        })
-        .style({
-          fill: function(d) {
-            var sum = d.Republican + d.Democrat
-            var percentD = d.Democrat / sum
-            return color(percentD)
-          },
-          "stroke-width": "1px",
-          stroke: function(d) {
-            return "#666"
-          },
-          "stroke-opacity": 1,
-          "fill-opacity": 0.8
-        })
-        .on("mouseover", function(d) {
-          div.transition()
-            .duration(200)
-            .style("opacity", .9);
-          div.html(d.act)
-            .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY - 28) + "px");
+        var node = svg.selectAll(".node")
+          .data(acts)
+        .enter().append("circle")
+          .attr({
+            cx: function(d) {
+              var sum = d.Republican + d.Democrat
+              var percentD = d.Democrat / sum
+              var scaled = d3.scale.linear()
+                .domain([1, 0])
+                .range([100, width - 100])
+              return scaled(percentD) + "px"
+            },
+            cy: height / 2 + "px",
+            r: function(d) { return (d.Republican + d.Democrat) },
+            class: 'node'
           })
-        .on("mouseout", function(d) {
-          div.transition()
-            .duration(500)
-            .style("opacity", 0);
-        });
-
-
-    })
-
-
-
-
-
-
-
-
+          .style({
+            fill: function(d) {
+              var sum = d.Republican + d.Democrat
+              var percentD = d.Democrat / sum
+              return color(percentD)
+            },
+            "stroke-width": "1px",
+            stroke: function(d) {
+              var sum = d.Republican + d.Democrat
+              var percentD = d.Democrat / sum
+              return color(percentD)
+              // return "#666"
+            },
+            "stroke-opacity": 1,
+            "fill-opacity": 0.8
+          })
+          .on("mouseover", function(d) {
+            div.transition()
+              .duration(200)
+              .style("opacity", .9);
+            div.html(d.act + " " + "(" + (d.Democrat+d.Republican) + ")")
+              .style("left", (d3.event.pageX) + "px")
+              .style("top", (d3.event.pageY - 28) + "px");
+            })
+          .on("mouseout", function(d) {
+            div.transition()
+              .duration(500)
+              .style("opacity", 0);
+          });
+      })
+    })()
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,132 +1,148 @@
-// Meteor.startup(function () {
-//
-//   /*
-//   README
-//   To run, uncomment the legislatorArray below, and populate bills at the bottom
-//
-//   */
-//
-//   var DOMParser = Meteor.npmRequire('xmldom').DOMParser
-//
-//   var fs = Npm.require('fs')
-//   var files = fs.readdirSync('/Users/xxxx/qSites/bills_xml')
-//
-//   let actSet = [];
-//
-//   console.log("Populating legislators...");
-//   let historicalLegislatorArray = JSON.parse(fs.readFileSync('/Users/xxxx/qSites/legislators_historical/legislators-historical.json', 'utf8'))
-//   let currentLegislatorArray = JSON.parse(fs.readFileSync('/Users/xxxx/qSites/legislators_historical/legislators-current.json', 'utf8'))
-//   let legislatorArray = historicalLegislatorArray.concat(currentLegislatorArray)
-//   console.log("Added " + legislatorArray.length + " legislators.");
-//
-//   // Must bind to the file system environment or it will not work
-//   var populateBills = Meteor.bindEnvironment(function(files) {
-//     // Iterate over each file
-//     _.each(files, function(file, i) {
-//       if (i < 1000) { // XXX this will eventually be removed. It's just to limit the number of times it runs
-//         // Find files that are type .json
-//         // that have been introduced in the house or senate
-//         var ihXML = /.+\ih.xml/;
-//         var isXML = /.+\is.xml/;
-//         // Only run this for the proper files
-//         if (ihXML.test(file) || isXML.test(file)) {
-//
-//           // function to get the proper value from XML
-//           var getValues = function(xmlDoc, entity) {
-//             let valueArray = [];
-//             // for each reference in the xml doc...
-//             _.each(xmlDoc, function(ref) {
-//               if (ref.getAttributeNode('entity-type').nodeValue === entity) {
-//                 // TODO this should not need to exist. They should all have values
-//                 if (ref.getAttributeNode('value')) {
-//                   // gets rid of all the extra stuff at the end of the common name
-//                   // if there isnt a tailing "/", then just return the item
-//                   let myRe = /(^.+?)\//
-//                   let reArray = myRe.exec(ref.getAttributeNode('value').nodeValue)
-//                   // push to the array
-//                   valueArray.push(reArray ? reArray[1] : ref.getAttributeNode('value').nodeValue)
-//                 }
-//               }
-//             })
-//             // return the array of referenced entities
-//             return valueArray;
-//           }
-//
-//           // Get the contents of the file
-//           var content = fs.readFileSync('/Users/xxxx/qSites/bills_xml/' + file, 'utf8');
-//
-//           var parser = new DOMParser();
-//           var xmlDoc = parser.parseFromString(content,"text/xml");
-//
-//           var sponsor = xmlDoc.getElementsByTagName("sponsor")[0].attributes[0].nodeValue
-//           var cosponsors = []
-//
-//           var entityRef = xmlDoc.getElementsByTagName('cato:entity-ref')
-//
-//           // only unique act names
-//           var acts = getValues(entityRef, 'act')
-//           var federalBodies = _.uniq(getValues(entityRef, 'federal-body'))
-//           // var people = getValues(entityRef, 'person')
-//           // var committees = getValues(entityRef, 'committee')
-//           // var uscodes = getValues(entityRef, 'uscode')
-//           // var publicLaws = getValues(entityRef, 'public-law')
-//           // var statutesAtLarge = getValues(entityRef, 'statute-at-large')
-//           // var billsByNumber = getValues(entityRef, 'bills-by-number')
-//
-//           // take only acts that reference other acts
-//           if (federalBodies.length) {
-//             // filter out all legislators other than the one we want
-//             let currentSponsor = _.find(legislatorArray, function(legislator) {
-//               return legislator.id.bioguide === sponsor;
-//             })
-//             // let sponsorName = currentSponsor.name.official_full
-//             let sponsorParty = currentSponsor.terms[0].party
-//
-//             // console.log(sponsorName, sponsorParty);
-//             _.each(federalBodies, function(act) {
-//               // Make sure the party is either republican or democrat
-//               if (sponsorParty === "Democrat" || sponsorParty === "Republican") {
-//                 // if it does not have the act
-//                 if (!_.find(actSet, function(obj) { return obj.act === act})) {
-//                   // give the object an "act" property
-//                   let actObj = {
-//                     act: act,
-//                     Republican: 0,
-//                     Democrat: 0
-//                   }
-//                   actObj[sponsorParty] += 1;
-//                   actSet.push(actObj)
-//
-//                 } else { // if the act has already been referenced
-//                   let currentAct = _.find(actSet, function(obj) {
-//                     return obj.act === act;
-//                   })
-//                   currentAct[sponsorParty] += 1;
-//                 }
-//               }
-//             })
-//           }
-//         }
-//       }
-//     });
-//   })
-//
-//   var resetDB = function(db) {
-//     _.each(db.find().fetch(), function(item) {
-//       db.remove(item._id)
-//     })
-//   }
-//
-//   // resetDB(Legislators)
-//
-//   console.log("Populating bills...");
-//   populateBills(files)
-//   console.log("Added " + files.length + " bills.");
-//
-//   // console.log(actSet);
-//
-//   // save file to desktop
-//   let res = fs.writeFileSync("/Users/xxxx/Desktop/federalBodies.json", JSON.stringify(actSet));
-//   console.log(res);
-//
-// });
+Meteor.startup(function () {
+
+  /*
+  README
+  To run, uncomment the legislatorArray below, and populate bills at the bottom
+
+  */
+
+  var DOMParser = Meteor.npmRequire('xmldom').DOMParser
+
+  var fs = Npm.require('fs')
+  var files = fs.readdirSync('/Users/xxxx/qSites/bills_xml')
+
+  let actSet = [];
+
+  console.log("Populating legislators...");
+  let historicalLegislatorArray = JSON.parse(fs.readFileSync('/Users/xxxx/qSites/legislators_historical/legislators-historical.json', 'utf8'))
+  let currentLegislatorArray = JSON.parse(fs.readFileSync('/Users/xxxx/qSites/legislators_historical/legislators-current.json', 'utf8'))
+  let legislatorArray = historicalLegislatorArray.concat(currentLegislatorArray)
+  console.log("Added " + legislatorArray.length + " legislators.");
+
+  // Must bind to the file system environment or it will not work
+  var populateBills = Meteor.bindEnvironment(function(files) {
+    // Iterate over each file
+    let counter = 0;
+
+    _.each(files, function(file, i) {
+      if (i < 1000) { // XXX this will eventually be removed. It's just to limit the number of times it runs
+        // Find files that are type .json
+        // that have been introduced in the house or senate
+        var ihXML = /.+\ih.xml/;
+        var isXML = /.+\is.xml/;
+        // Only run this for the proper files
+        if (ihXML.test(file) || isXML.test(file)) {
+
+          // function to get the proper value from XML
+          var getValues = function(xmlDoc, entity) {
+            let valueArray = [];
+            // for each reference in the xml doc...
+            _.each(xmlDoc, function(ref) {
+              if (ref.getAttributeNode('entity-type').nodeValue === entity) {
+                // TODO this should not need to exist. They should all have values
+                if (ref.getAttributeNode('value')) {
+                  // Exclusing bills with "proposed" value of "true"
+                  if (!ref.getAttributeNode('proposed')) {
+                    let myRe = /(^.+?)\//
+                    let reArray = myRe.exec(ref.getAttributeNode('value').nodeValue)
+                    // push to the array
+                    valueArray.push(reArray ? reArray[1] : ref.getAttributeNode('value').nodeValue)
+                  }
+                  // gets rid of all the extra stuff at the end of the common name
+                  // if there isnt a tailing "/", then just return the item
+                  /*
+                  for Federal bodies
+                  entity-parent-id
+                  and
+                  entity-id
+                  Then we references the entity-id against the table
+                  */
+
+
+                } else {
+                  // This is where we find things that don't work
+                }
+              }
+            })
+            // return the array of referenced entities
+            return valueArray;
+          }
+
+          // Get the contents of the file
+          var content = fs.readFileSync('/Users/xxxx/qSites/bills_xml/' + file, 'utf8');
+
+          var parser = new DOMParser();
+          var xmlDoc = parser.parseFromString(content,"text/xml");
+
+          var sponsor = xmlDoc.getElementsByTagName("sponsor")[0].attributes[0].nodeValue
+          var cosponsors = []
+
+          var entityRef = xmlDoc.getElementsByTagName('cato:entity-ref')
+
+          // only unique act names
+          var acts = getValues(entityRef, 'act')
+          // var federalBodies = _.uniq(getValues(entityRef, 'federal-body'))
+          // var people = getValues(entityRef, 'person')
+          // var committees = getValues(entityRef, 'committee')
+          // var uscodes = getValues(entityRef, 'uscode')
+          // var publicLaws = getValues(entityRef, 'public-law')
+          // var statutesAtLarge = getValues(entityRef, 'statute-at-large')
+          // var billsByNumber = getValues(entityRef, 'bills-by-number')
+
+          // take only acts that reference other acts
+          if (acts.length) {
+            // filter out all legislators other than the one we want
+            let currentSponsor = _.find(legislatorArray, function(legislator) {
+              return legislator.id.bioguide === sponsor;
+            })
+            // let sponsorName = currentSponsor.name.official_full
+            let sponsorParty = currentSponsor.terms[0].party
+
+            // console.log(sponsorName, sponsorParty);
+            _.each(acts, function(act) {
+              // Make sure the party is either republican or democrat
+              if (sponsorParty === "Democrat" || sponsorParty === "Republican") {
+                // if it does not have the act
+                if (!_.find(actSet, function(obj) { return obj.act === act})) {
+                  // give the object an "act" property
+                  let actObj = {
+                    act: act,
+                    Republican: 0,
+                    Democrat: 0
+                  }
+                  actObj[sponsorParty] += 1;
+                  actSet.push(actObj)
+
+                } else { // if the act has already been referenced
+                  let currentAct = _.find(actSet, function(obj) {
+                    return obj.act === act;
+                  })
+                  currentAct[sponsorParty] += 1;
+                }
+              }
+            })
+          }
+        }
+      }
+    });
+  })
+
+  var resetDB = function(db) {
+    _.each(db.find().fetch(), function(item) {
+      db.remove(item._id)
+    })
+  }
+
+  // resetDB(Legislators)
+
+  console.log("Populating bills...");
+  populateBills(files)
+  console.log("Added " + files.length + " bills.");
+
+  // console.log(actSet);
+
+  // save file to desktop
+  let res = fs.writeFileSync("/Users/xxxx/Desktop/acts.json", JSON.stringify(actSet));
+  console.log(res);
+
+});
